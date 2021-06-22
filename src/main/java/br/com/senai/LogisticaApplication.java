f61dc3a5670cd3053e489678ba1537205fcfccce
RA4Z <rraz639@gmail.com>
2021-06-22 16:37:24 -0300

    Segurança do programa

--- src/main/java/br/com/senai/LogisticaApplication.java
+++ src/main/java/br/com/senai/LogisticaApplication.java
@@ -3,12 +3,14 @@ package br.com.senai;
 import br.com.senai.api.controller.PessoaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LogisticaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticaApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		System.out.println("macarrão");
 	}
 
