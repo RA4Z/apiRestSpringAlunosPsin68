dced0a99689ca7bc6353443f5b0f07f20153d7eb
RA4Z <rraz639@gmail.com>
2021-06-28 11:40:25 -0300

    Login Finalizado

--- src/main/java/br/com/senai/security/Auth.java
+++ src/main/java/br/com/senai/security/Auth.java
@@ -2,5 +2,5 @@ package br.com.senai.security;
 
 public class Auth {
     final static String secret = "e60f68ade7ebba90933749b079b31cb3";
-    final static int expiresIn = 120000;
+    final static int expiresIn = 7200000;
 }
