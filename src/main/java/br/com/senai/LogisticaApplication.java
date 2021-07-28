9169e14994666d088f5a1bcc5b9f698ded355d8f
RA4Z <rraz639@gmail.com>
2021-07-28 14:44:01 -0300

    Cors na segurança

--- src/main/java/br/com/senai/LogisticaApplication.java
+++ src/main/java/br/com/senai/LogisticaApplication.java
@@ -11,6 +11,7 @@ public class LogisticaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticaApplication.class, args);
 		System.out.println("macarrão");
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 
 }
